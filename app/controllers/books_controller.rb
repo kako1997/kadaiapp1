dfc1071c4b707cc3d207bd1715ab224661707222
EC2 Default User <ec2-user@ip-172-31-44-213.ap-northeast-1.compute.internal>
2023-05-20 12:24:05 +0000

    c

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,46 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
     @book = Book.new(book_params)
+     @books = Book.all
     if @book.save
+      flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
-     render :new
+      render:index
     end
     
   end
 
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy 
+    flash[:notice] ="Book was successfully destroyed."
+    redirect_to '/books'
+  end
+
   def index
-    @books = book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+     @book = Book.find(params[:id])
   end
 
   def edit
+     @book = Book.find(params[:id])
   end 
   
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    flash[:notice] ="Book was successfully updated."
+    redirect_to book_path(@book.id)  
+     else
+      render:edit
+    end
+  end
   
    private
    
